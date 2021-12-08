040e00d1ec721f53550b62ae35558a1b66155808
AlbertoVf <8244709-AlbertoVf1@users.noreply.gitlab.com>
2021-12-08 12:56:58 +0100

    Update installer

--- install_software.sh
+++ install_software.sh
@@ -7,6 +7,7 @@ info_instalation() {
 is_installed() {
   tput setaf 2; printf "\n[ $(date) ]  The package $1 is already installed\n"; tput sgr0
 }
+
 is_not_installed() {
   tput setaf 3; printf "\n[ $(date) ]  Installing package $1\n"; tput sgr0
 }
@@ -23,13 +24,18 @@ install_community() {
   done
 }
 
-install_fonts() {
-  info_instalation "Installing fonts"; fonts=($(cat font.list))
-  for name in "${fonts[@]}"; do
+install_aur() {
+  dest="$(xdg-user-dir DOWNLOAD)/aur"
+  info_instalation "Installing aur"; aur=($(cat aur.list))
+  mkdir $dest || cd $dest;
+  for name in "${aur[@]}" ; do
+    cd $dest || exit;
     if pacman -Qi $name &> /dev/null; then
       is_installed $name
     else
-      is_not_installed $name;paru $name
+      is_not_installed $name
+      git clone https://aur.archlinux.org/$name.git
+      cd $name && makepkg -si --needed --noconfirm
     fi
   done
 }
@@ -46,20 +52,6 @@ install_snap() {
   done
 }
 
-install_aur() {
-  info_instalation "Installing aur"; aur=($(cat aur.list))
-  mkdir ~/aur
-  for name in "${aur[@]}" ; do
-    cd ~/aur || exit;
-    if pacman -Qi $name &> /dev/null; then
-      is_installed $name
-    else
-      is_not_installed $name
-      git clone https://aur.archlinux.org/$name.git && cd $name && makepkg -si --needed --noconfirm
-    fi
-  done
-}
-
 install_lang() {
   info_instalation "Installing lang"; lang=($(cat lang.list))
   for name in "${lang[@]}" ; do
@@ -68,17 +60,6 @@ install_lang() {
     else
       is_not_installed $name
       sudo pacman -Sy --noconfirm --needed $name
-    fi
-  done
-}
-
-install_themes() {
-  info_instalation "Installing icons/themescursor"; fonts=($(cat theme.list))
-  for name in "${fonts[@]}"; do
-    if pacman -Qi $name &> /dev/null; then
-      is_installed $name
-    else
-      is_not_installed $name;paru $name
     fi
   done
 }
