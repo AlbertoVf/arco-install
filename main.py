b02435bba2ee33148bc0439fff79a878607895d0
AlbertoVf <avf.git-repos@protonmail.com>
2022-03-30 16:54:30 +0200

    Update software

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
         red()
         print("\n[!] Es necesario ser usuario root para ejecutar este script")
     else:
-        os.system('sudo reflector - f 30 - l 30 - -number 10 - -verbose - -save / etc/pacman.d/mirrorlist')
+        os.system(
+            'sudo reflector - f 30 - l 30 - -number 10 - -verbose - -save / etc/pacman.d/mirrorlist')
         os.system('sudo pacman -Sy')
         menu()
