0c37d86b42d9bad062b5c1a324ab7eeb17cc6d1f
AlbertoVf <avf.git-repos@protonmail.com>
2022-04-08 17:13:12 +0200

    Update installer

--- installer.py
+++ installer.py
@@ -2,31 +2,13 @@ import os
 import json
 import time
 
-from sys import stdout
 
-
-def red():
-    stdout.write("\033[1;31m")
-
-
-def green():
-    stdout.write("\033[0;32m")
-
-
-def blue():
-    stdout.write("\033[1;34m")
-
-
-def yellow():
-    stdout.write("\033[1;33m")
-
-
-def purple():
-    stdout.write("\033[1;35m")
-
-
-def white():
-    stdout.write("\033[1;37m")
+red = "\033[1;31m"
+green = "\033[0;32m"
+blue = "\033[1;34m"
+yellow = "\033[1;33m"
+purple = "\033[1;35m"
+white = "\033[1;37m"
 
 
 def is_installed(package: str):
@@ -40,38 +22,35 @@ def read_software(type: str):
 
 
 def instalacion_ok():
-    blue()
     time.sleep(1)
-    print("[+] Instalacion completa\n")
+    print(f"{blue}[+] Instalacion completa\n")
 
 
 def init_installation(msg: str):
-    yellow()
-    print(f'{msg}\n')
-    white()
+    print(f'{yellow}{msg}\n{white}')
 
 
 def menu():
-    yellow()
-    print("""
+    print(f"""{yellow}
     _                 ___           _        _ _
    / \   _ __ ___ ___|_ _|_ __  ___| |_ __ _| | |
   / _ \ | '__/ __/ _ \| || '_ \/ __| __/ _` | | |
  / ___ \| | | (_| (_) | || | | \__ \ || (_| | | |
 /_/   \_\_|  \___\___/___|_| |_|___/\__\__,_|_|_|
 """)
-    purple()
-    print(""" [1] Instalacion completa\n [2] Instalacion de paquetes community\n [3] Instalacion de paquetes aur\n [4] Instalacion de paquetes snap\n [5] Instalacion de paquetes extra\n [6] Salir\n """)
-    blue()
-    option = input("\n[*] Seleccione una opcion: ")
+    print(f"""{purple} [1] Instalacion completa\n [2] Instalacion de paquetes community\n [3] Instalacion de paquetes aur\n [4] Instalacion de paquetes snap\n [5] Instalacion de paquetes extra\n [6] Salir\n """)
+
+    option = input(f"\n{blue}[*] Seleccione una opcion: ")
 
     if option == "1":
         aur()
-        community()
+        community("community")
+        community("distro")
         snap()
         extra()
     if option == "2":
-        community()
+        community("community")
+        community("distro")
     if option == "3":
         aur()
     if option == "4":
@@ -84,47 +63,37 @@ def menu():
 
 def aur():
     init_installation("Instalando paquetes de AUR...")
-
     for i in read_software('aur'):
         if is_installed(i):
-            green()
-            print(f'[-] {i} ya esta instalado')
+            print(f'{green}[-] {i} ya esta instalado')
         else:
-            yellow()
-            print(f'[+] Instalando {i}')
-            os.system(f'git clone https://aur.archlinux.org/{i}.git')
-            os.system(f'cd {i}')
-            os.system('makepkg -si --needed --noconfirm')
-            os.system(f'cd ..')
+            print(f'{yellow}[+] Instalando {i}')
+            os.system(
+                f'git clone https://aur.archlinux.org/{i}.git && cd {i} && makepkg -si --noconfirm && cd ..')
     instalacion_ok()
 
 
 def snap():
     init_installation("Instalando paquetes snap...")
-
-    os.system('systemctl enable --now snapd.socket')
-    os.system('ln -s /var/lib/snapd/snap /snap')
+    os.system(
+        'systemctl enable --now snapd.socket && ln -s /var/lib/snapd/snap /snap')
 
     for i in read_software('snap'):
         if os.system(f'snap list | grep {i}') == 0:
-            green()
-            print(f'[-] {i} ya esta instalado')
+            print(f'{green}[-] {i} ya esta instalado')
         else:
-            yellow()
-            print(f'[+] Instalando {i}')
+            print(f'{yellow}[+] Instalando {i}')
             os.system(f'snap install {i}')
     instalacion_ok()
 
 
-def community():
+def community(software="community"):
     init_installation("Instalando paquetes de la comunidad...")
-    for i in read_software('community'):
+    for i in read_software(software):
         if is_installed(i):
-            green()
-            print(f'[-] {i} ya esta instalado')
+            print(f'{green}[-] {i} ya esta instalado')
         else:
-            yellow()
-            print(f'[+] Instalando {i}')
+            print(f'{yellow}[+] Instalando {i}')
             os.system(f'pacman -Sy --noconfirm --needed {i}')
     instalacion_ok()
 
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
-from installer import menu, red
+from installer import menu
 
 if __name__ == '__main__':
     id = os.getuid()
 
     if id == 0:
-        red()
-        print("\n[!] Es necesario ser usuario root para ejecutar este script")
+        print("\n\033[1;31m[!] Es necesario ser root para ejecutar el script")
     else:
         os.system(
-            'sudo reflector - f 30 - l 30 - -number 10 - -verbose - -save / etc/pacman.d/mirrorlist')
-        os.system('sudo pacman -Sy')
+            'sudo reflector -f 30 -l 30 --number 10 --save /etc/pacman.d/mirrorlist && sudo pacman -Sy')
         menu()
