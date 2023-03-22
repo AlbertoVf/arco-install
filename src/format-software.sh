9d41d5f1de15e190cbb5156f9b2ea3c9a0fcfe77
AlbertoVf <avf.git-repos@protonmail.com>
2023-03-22 21:18:16 +0100

    ✨ feat(format): replace flags
    update script to format software

--- src/format-software.sh
+++ src/format-software.sh
@@ -5,19 +5,10 @@ csv_file="$software_root/software.csv"
 json_file="$software_root/software.json"
 
 function format_software() {
-
-	sed -Ee 's/(\s+;)/;/g' \
-		-i $csv_file
-
-	jq --slurp --raw-input --raw-output \
-		'split("\n") | .[1:] | map(split(";")) | map({"repositorio": .[0], "tags": .[1], "paquete": .[2]})' \
+	jq -sRrc 'split("\n") | .[1:] | map(split(";")) | map({"repositorio": .[0], "tags": .[1], "paquete": .[2]})' \
 		$csv_file >$json_file
 
-	jq '.[] | select(.repositorio=="community") | .paquete' $json_file >"$software_root/community.json"
-	jq '.[] | select(.repositorio=="aur") | .paquete' $json_file >"$software_root/aur.json"
-	jq '.[] | select(.repositorio=="snap") | .paquete' $json_file >"$software_root/snap.json"
-	jq '.[] | select(.repositorio=="distro") | .paquete' $json_file >"$software_root/distro.json"
-	jq '.[] | select(.repositorio=="extra") | .paquete' $json_file >"$software_root/extra.json"
-}
+	sed -Ee 's/(\s+\")/\"/g' -i $json_file
 
+}
 format_software
