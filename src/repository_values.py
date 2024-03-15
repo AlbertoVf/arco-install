07d006934011fac01ec0abd7f2df04673c7b937c
AlbertoVf <avf.git-repos@protonmail.com>
2024-03-15 22:06:43 +0100

    refactor: extract to file
    extract constant to external file

---
+++ src/repository_values.py
@@ -0,0 +1,5 @@
+class RepositoryValues:
+    DISTRO = ["distro", "community", "extra"]
+    COMPILABLE = ["aur", "snap", "flatpack"]
+    SCRIPT = ["script"]
+    ALL = DISTRO + COMPILABLE + SCRIPT
---
+++ src/software_keys.py
@@ -0,0 +1,6 @@
+# The class `SoftwareKeys` defines constants for keys related to software information.
+class SoftwareKeys:
+    REPOSITORY = "Repository"
+    NAME = "Name"
+    TAGS = "Tags"
+    SOFTWARE = "Software"
