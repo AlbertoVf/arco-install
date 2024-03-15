b49335891c9c6d60ab8eddc3a5f3c9923e74de16
AlbertoVf <avf.git-repos@protonmail.com>
2024-03-15 18:35:50 +0100

    perf: use list to install software by type

--- arco_install.py
+++ arco_install.py
@@ -1,45 +1,33 @@
 import argparse
 import subprocess
-from src.installer import *
+from src.installer import install, clear_cache, update, export_scripts
+
+
+class REPOSITORY_TAGS:
+    DISTRO = ["distro", "community", "extra"]
+    COMPILABLE = ["aur", "snap", "flatpack"]
+    SCRIPT = ["script"]
+    ALL = DISTRO + COMPILABLE + SCRIPT
 
 
 def main():
     parser = argparse.ArgumentParser(description="Install the required packages")
     parser.add_argument("-a", "--all", action="store_true", help="Install all software")
-    parser.add_argument(
-        "-c",
-        "--compilable",
-        action="store_true",
-        help="Install software from AUR, snap",
-    )
-    parser.add_argument(
-        "-d",
-        "--distro",
-        action="store_true",
-        help="Install software from distribuiton and communnity",
-    )
-    parser.add_argument(
-        "-e", "--extra", action="store_true", help="Install extra software"
-    )
+    parser.add_argument( "-c", "--compilable", action="store_true", help="Install software from AUR, snap", )
+    parser.add_argument( "-d", "--distro", action="store_true", help="Install software from distribution and communnity", )
+    parser.add_argument( "-s", "--script", action="store_true", help="Install extra software" )
     args = parser.parse_args()
 
+    update()
     if args.distro:
-        install("distro")
-        install("community")
-        install("extra")
-    if args.extra:
-        install("command")
+        [install(r) for r in REPOSITORY_TAGS.DISTRO]
+    if args.script:
+        [install(r) for r in REPOSITORY_TAGS.SCRIPT]
     if args.compilable:
-        install("aur")
-        install("snap")
+        [install(r) for r in REPOSITORY_TAGS.COMPILABLE]
     if args.all:
-        install("distro")
-        install("community")
-        install("extra")
-        install("aur")
-        install("snap")
-        install("command")
+        [install(r) for r in REPOSITORY_TAGS.ALL]
+    clear_cache()
 
 
-update()
 main()
