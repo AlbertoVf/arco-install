882d75060f2178cdc0e8fd79c918308b4deaec28
AlbertoVf <avf.git-repos@protonmail.com>
2024-04-23 19:51:15 +0200

    feat: check args
    check if receive any arg

--- arco_install.py
+++ arco_install.py
@@ -1,18 +1,19 @@
 import argparse
-import subprocess
 from src.format_software import export_to_file
 from src.installer import install, clear_cache, update, export_scripts
 from src.repository_values import RepositoryValues as rv
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Install the required packages")
+    parser = argparse.ArgumentParser(
+        description="Install the required packages", exit_on_error=True
+    )
     parser.add_argument("-a", "--all", action="store_true", help="Install all software")
     parser.add_argument(
         "-c",
         "--compilable",
         action="store_true",
-        help="Install software from AUR, snap",
+        help="Install software from AUR, snap...",
     )
     parser.add_argument(
         "-d",
@@ -21,14 +22,19 @@ def main():
         help="Install software from distribution and communnity",
     )
     parser.add_argument(
-        "-s", "--script", action="store_true", help="Install extra software"
+        "-s", "--script", action="store_true", help="Run software by command line"
     )
     parser.add_argument(
         "-e", "--export", action="store_true", help="Build bashscript installation file"
     )
     args = parser.parse_args()
 
-    export_to_file()
+    if all(value == False for value in vars(args).values()):
+        parser.print_help()
+        return
+    else:
+        export_to_file()
+
     if args.export:
         export_scripts(rv.ALL)
     else:
